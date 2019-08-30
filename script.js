4b6f74c0dea5326e8baa3630160c3917406bff55
CN72 <cn72@CN72.local>
2019-08-30 16:49:49 +0100

    latest additions

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let inputContainer = document.getElementById("input-container")
+let playerNumber = document.getElementById("player-number")
+let gameField = document.getElementById("game-field")
 
+const gameStart = () => {
+    inputContainer.style.display = "block";
+    playerNumber.value = ""
+}
+
+const submit = () => {
+    if(playerNumber.value > 0){
+    inputContainer.style.display = "none";
+    console.log(playerNumber.value) 
+    let player = document.createElement("div") 
+    player.innerHTML = "player 3"
+    document.body.gameField.appendChild(player)
+}
+    else {
+        playerNumber.value = "Please type a number"
+    }
+}
+
+window.addEventListener("keypress", () => {
+    if(event.which == 13){
+        submit()
+    }
+})
+
+window.addEventListener("keypress", () => {
+    if(event.which == 32){
+        gameStart ()
+        console.log("May the best gambler win!")
+    }
+})
+
+playerNumber.addEventListener("click", () => {
+    playerNumber.value = ""
+})
