8f8468239873d168f62d1baa6bef874a4ca611e8
CN72 <cn72@CN72.local>
2019-09-02 14:49:25 +0100

    added more logic to submit and game start functions

--- script.js
+++ script.js
@@ -3,17 +3,42 @@ let playerNumber = document.getElementById("player-number")
 let gameField = document.getElementById("game-field")
 
 const gameStart = () => {
+    document.location.reload()
     inputContainer.style.display = "block";
     playerNumber.value = ""
+    
 }
 
 const submit = () => {
     if(playerNumber.value > 0){
+
+        for (i = 1; i <= playerNumber.value; i++){
     inputContainer.style.display = "none";
+
     console.log(playerNumber.value) 
+
     let player = document.createElement("div") 
-    player.innerHTML = "player 3"
-    document.body.appendChild(player)
+    let playerTitle = document.createElement("h3")
+    playerTitle.innerHTML = `Player ${i}`
+    let playerScore = document.createElement("p")
+    playerScore.innerHTML = "0"
+    let divImage = document.createElement("div") 
+    let diceImage = document.createElement("img")
+    let rollBtn = document.createElement("button")
+    rollBtn.innerHTML = "Roll"
+    
+    gameField.appendChild(player)
+    player.appendChild(playerTitle)
+    player.appendChild(playerScore)
+    player.appendChild(divImage)
+    divImage.appendChild(diceImage)
+    player.appendChild(rollBtn)
+
+  
+
+    player.id = `player-${i}`
+    
+        }
 }
     else {
         playerNumber.value = "Please type a number"
@@ -36,3 +61,5 @@ window.addEventListener("keypress", () => {
 playerNumber.addEventListener("click", () => {
     playerNumber.value = ""
 })
+
+
